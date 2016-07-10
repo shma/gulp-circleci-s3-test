a3832123b13d531b7f7fccac77fed74a3c56fd1d
Shunya Matsuno <id.shma@gmail.com>
2016-07-10 22:56:25 +0900

    change settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");
 var markdown = require("gulp-markdown");
-
 var imagemin = require("gulp-imagemin");
+var fs = require("fs");
 
 gulp.task("imageMinTask", function() {
  gulp.src("images/*.png")
@@ -13,6 +13,15 @@ gulp.task('default', function() {
     return gulp.src('intro.md')
         .pipe(markdown())
         .pipe(gulp.dest('dist'));
-         
 })
 
+
+/**
+var s3 = require("gulp-s3");
+ 
+gulp.task('s3', function() {
+  aws = JSON.parse(fs.readFileSync('./aws.json'));
+  return gulp.src('./s3/**')
+    .pipe(s3(aws));
+})
+**/
